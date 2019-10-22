c12edeb83917fba6f0f1174c628f0236851bd156
Adam Kent <Adam@Adams-MacBook-Pro-2.local>
2019-10-22 10:45:44 -0700

    fixed images

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Product from './Product'
 
 export default function Dashboard(props) {
-    const mappedProducts = props.inventory.map(product => {
-        return(<Product product={product}/>)
+    const mappedProducts = props.inventory.map((product, index) => {
+        return(
+        <div key={index}>
+            <Product product={product}/>
+        </div>
+        )
     })
     return (
         <div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export default function Product(props) {
+    console.log(props)
     return (
         <div>
             <h1>{props.product.name}</h1>
             <p>{props.product.price}</p>
-            <img alt='productimg' src={props.product.imgurl}/>
+            <img className="product-img" alt='productimg' src={props.product.img}/>
         </div>
     )
 };
